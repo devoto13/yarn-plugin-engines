171eb02739bdfd47a8b854e6d79aaa50a64d84fe
romdej <romain@romdej.dev>
2022-04-17 22:06:38 +0200

    refactor: remove the code duplication from the hooks

--- src/index.ts
+++ src/index.ts
@@ -10,46 +10,37 @@ import { readFileSync } from "fs";
 import { resolve } from "path";
 import { satisfies } from "semver";
 
+const verifyEngines = (reportError: (message: string) => never) => (project: Project): void => {
+  const packageJson = readFileSync(
+    resolve(npath.fromPortablePath(project.cwd), "package.json"),
+    "utf-8"
+  );
+  const { engines = {} } = JSON.parse(packageJson);
+  if (engines.node != null && !satisfies(process.version, engines.node)) {
+    reportError(
+      `The current Node version ${process.version} does not satisfy the required version ${engines.node}.`
+    );
+  }
+  if (engines.yarn != null && !satisfies(YarnVersion, engines.yarn)) {
+    reportError(
+      `The current Yarn version v${YarnVersion} does not satisfy the required version ${engines.yarn}.`
+    );
+  }
+}
+
+const reportYarnError = (message: string) => {
+  throw new ReportError(MessageName.UNNAMED, message);
+}
+
+const reportConsoleError = (message: string) => {
+  console.error(message);
+  process.exit(1);
+}
+
 const plugin: Plugin = {
   hooks: {
-    validateProject: (project: Project): void => {
-      const packageJson = readFileSync(
-        resolve(npath.fromPortablePath(project.cwd), "package.json"),
-        "utf-8"
-      );
-      const { engines = {} } = JSON.parse(packageJson);
-      if (engines.node != null && !satisfies(process.version, engines.node)) {
-        throw new ReportError(
-          MessageName.UNNAMED,
-          `The current Node version ${process.version} does not satisfy the required version ${engines.node}.`
-        );
-      }
-      if (engines.yarn != null && !satisfies(YarnVersion, engines.yarn)) {
-        throw new ReportError(
-          MessageName.UNNAMED,
-          `The current Yarn version v${YarnVersion} does not satisfy the required version ${engines.yarn}.`
-        );
-      }
-    },
-    setupScriptEnvironment: async (project: Project): Promise<void> => {
-      const packageJson = readFileSync(
-        resolve(npath.fromPortablePath(project.cwd), "package.json"),
-        "utf-8"
-      );
-      const { engines = {} } = JSON.parse(packageJson);
-      if (engines.node != null && !satisfies(process.version, engines.node)) {
-        console.error(
-          `The current Node version ${process.version} does not satisfy the required version ${engines.node}.`
-        );
-        process.exit(1);
-      }
-      if (engines.yarn != null && !satisfies(YarnVersion, engines.yarn)) {
-        console.error(
-          `The current Yarn version v${YarnVersion} does not satisfy the required version ${engines.yarn}.`
-        );
-        process.exit(1);
-      }
-    },
+    validateProject: verifyEngines(reportYarnError),
+    setupScriptEnvironment: verifyEngines(reportConsoleError),
   },
 };
 
