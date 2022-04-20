ec736040ec2c41786018d5461e4e5086d57151a9
Yaroslav Admin <devoto13@gmail.com>
2022-04-20 21:10:52 +0200

    Format code base using Prettier

--- src/engine-checkers/yarn.engine-checker.ts
+++ src/engine-checkers/yarn.engine-checker.ts
@@ -3,18 +3,17 @@ import { YarnVersion } from "@yarnpkg/core";
 import { EngineChecker } from "./engine-checker";
 
 export class YarnEngineChecker extends EngineChecker {
-    get engine(): string {
-        return "Yarn";
-    }
+  get engine(): string {
+    return "Yarn";
+  }
 
-    verifyEngine(engines: Record<string, string>): void {
-        const yarnRequiredVersion = engines.yarn;
-        if (yarnRequiredVersion == null) {
-            return;
-        }
-        if (!satisfies(YarnVersion, yarnRequiredVersion)) {
-            this.throwWrongEngineError(YarnVersion, yarnRequiredVersion);
-        }
+  verifyEngine(engines: Record<string, string>): void {
+    const yarnRequiredVersion = engines.yarn;
+    if (yarnRequiredVersion == null) {
+      return;
     }
-
+    if (!satisfies(YarnVersion, yarnRequiredVersion)) {
+      this.throwWrongEngineError(YarnVersion, yarnRequiredVersion);
+    }
+  }
 }
